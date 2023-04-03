26adbbf1f3caecefe0b5022e9c6905b2c76588ed
Antony Vasiliev <tony.vasiliev8@gmail.com>
2023-04-03 00:50:38 -0400

    Added base ejs views

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ console.clear();
 
 const express = require('express');
 const http = require('http');
+const fs = require('fs');
 
 const DEFAULT_PORT = 8080;
+const CONFIG_PATH = `${__dirname}/config.json`
+
 process.env.HTTP_PORT = process.env.HTTP_PORT || DEFAULT_PORT;
 
 let app = express();
 let server = http.createServer(app);
+
+app.set('view engine', 'ejs');
 
 server.listen(process.env.HTTP_PORT, () => {
     console.log('HTTP server running!');
@@ -19,5 +24,30 @@ app.all('*', (request, response, next) => {
 })
 
 app.get('/', (request, response) => {
-    response.end('Oi!')
+    getConfig().then(config => {
+        response.render('home', {config});
+    }).catch(error => {
+        response.render('500', {error});
+    })
 })
+
+app.get('*', (request, response) => {
+    response.render('404', {url: request.url});
+})
+
+function getConfig(){
+    return new Promise((resolve, reject) => {
+        fs.readFile(CONFIG_PATH, 'utf-8', (err, data) => {
+            if (err){
+                reject(err)
+            }
+            else{
+                try {
+                    resolve(JSON.parse(data));
+                } catch (error) {
+                    reject(new Error('Failed to parse config json'))
+                }
+            }
+        })
+    })
+}
